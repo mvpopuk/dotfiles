680c112d4e1c119f379101df6c2a4f159975cc3b
Marian Pop <mvpopuk@gmail.com>
2022-04-18 19:03:06 +0300

    wip

--- nvim/config/lualine.lua
+++ nvim/config/lualine.lua
@@ -1,30 +1,30 @@
 -- -- Dracula Colors
--- local colors = {
---   white = '#feffff',
---   red = '#ff6d67',
---   grey = '#abb2bf',
---   gray = '#55596d',
---   black = '#323844',
---   transparent = '#282a36',
---   green = '#59f68d',
---   yellow = '#f3f89d',
---   blue = '#61afef',
---   magenta = '#ff92d0',
---   cyan = '#99ecfd',
---   orange = '#ff8700',
--- }
+local colors = {
+  white = '#feffff',
+  red = '#ff6d67',
+  grey = '#abb2bf',
+  gray = '#55596d',
+  black = '#323844',
+  transparent = '#282a36',
+  green = '#59f68d',
+  yellow = '#f3f89d',
+  blue = '#61afef',
+  magenta = '#ff92d0',
+  cyan = '#99ecfd',
+  orange = '#ff8700',
+}
 
--- local dracula = {
---   normal = {
---     a = { fg = colors.grey, bg = colors.black },
---     b = { fg = colors.grey,  bg = colors.black },
---     c = { fg = colors.gray, bg = colors.transparent },
---     z = { fg = colors.grey, bg = colors.black },
---   },
---   insert = { a = { fg = colors.transparent, bg = colors.green } },
---   visual = { a = { fg = colors.transparent, bg = colors.yellow } },
---   replace = { a = { fg = colors.transparent, bg = colors.red } },
--- }
+local dracula = {
+  normal = {
+    a = { fg = colors.grey, bg = colors.black },
+    b = { fg = colors.grey,  bg = colors.black },
+    c = { fg = colors.gray, bg = colors.transparent },
+    z = { fg = colors.grey, bg = colors.black },
+  },
+  insert = { a = { fg = colors.transparent, bg = colors.green } },
+  visual = { a = { fg = colors.transparent, bg = colors.yellow } },
+  replace = { a = { fg = colors.transparent, bg = colors.red } },
+}
 
 -- One Dark Pro Colors
 -- local colors = {
@@ -109,32 +109,32 @@
 -- }
 
 -- Carbon Colors
-local colors = {
-  red = '#db6e89',
-  grey = '#f5f5f5',
-  gray = '#4c556a',
-  light_grey = '#979BAC',
-  black = '#243354',
-  white = '#ffffff',
-  transparent = '#172030',
-  light_green = '#83a598',
-  orange = '#fe8019',
-  green = '#73a7a7',
-  yellow = '#ffae8f',
-  cyan = '#03858e',
-}
+-- local colors = {
+--   red = '#db6e89',
+--   grey = '#6b7089',
+--   gray = '#4c556a',
+--   light_grey = '#979BAC',
+--   black = '#1e2a3f',
+--   white = '#ffffff',
+--   transparent = '#172030',
+--   light_green = '#83a598',
+--   orange = '#fe8019',
+--   green = '#73a7a7',
+--   yellow = '#ffae8f',
+--   cyan = '#03858e',
+-- }
 
-local carbon = {
-  normal = {
-    a = { fg = colors.grey, bg = colors.black },
-    b = { fg = colors.grey,  bg = colors.black },
-    c = { fg = colors.gray, bg = colors.transparent },
-    z = { fg = colors.grey, bg = colors.black },
-  },
-  insert = { a = { fg = colors.transparent, bg = colors.green } },
-  visual = { a = { fg = colors.transparent, bg = colors.yellow } },
-  replace = { a = { fg = colors.transparent, bg = colors.red } },
-}
+-- local carbon = {
+--   normal = {
+--     a = { fg = colors.grey, bg = colors.black },
+--     b = { fg = colors.grey,  bg = colors.black },
+--     c = { fg = colors.gray, bg = colors.transparent },
+--     z = { fg = colors.grey, bg = colors.black },
+--   },
+--   insert = { a = { fg = colors.transparent, bg = colors.green } },
+--   visual = { a = { fg = colors.transparent, bg = colors.yellow } },
+--   replace = { a = { fg = colors.transparent, bg = colors.red } },
+-- }
 
 local empty = require('lualine.component'):extend()
 function empty:draw(default_highlight)
@@ -190,7 +190,7 @@ local git_blame = require('gitblame')
 
 require('lualine').setup {
     options = {
-    theme = carbon,
+    theme = dracula,
     component_separators = '',
     globalstatus = true,
   },
--- nvim/config/theme.vim
+++ nvim/config/theme.vim
@@ -69,30 +69,30 @@ colorscheme dracula
 
 " ------------------------------ Carbon Theme Overrides --------------------------- "
  
-function! CarbonOverrides() abort
-    highlight Normal guibg=None guifg=None
-    highlight NonText guibg=None guifg=None
-    highlight EndOfBuffer guifg=#1B253B
-    highlight CursorLine guibg=#1B253B
-    highlight CursorLineNr guibg=#1B253B guifg=#73a7a7
-    highlight VertSplit gui=NONE guibg=None guifg=#10737B
-    highlight IndentBlanklineChar guifg=#1B253B gui=nocombine
-    highlight IndentBlanklineContextChar guifg=#243353 gui=nocombine
-    highlight FloatermBorder guibg=None guifg=#10737B
-    highlight BufferLineSeparator guifg=#172030
-    highlight TroubleSignOther guibg=None guifg=green
-    highlight TroubleFoldIcon guifg=None
-    highlight TroubleCount guibg=None
-    highlight TelescopeBorder guifg=#243353
-    highlight TelescopePromptBorder guifg=#243353
-    highlight TelescopeResultsBorder guifg=#243353
-    highlight EndOfBuffer guifg=#172030
-endfunction
-augroup Colors
-    autocmd!
-    autocmd ColorScheme carbon call CarbonOverrides()
-augroup END
+" function! CarbonOverrides() abort
+"     highlight Normal guibg=None guifg=None
+"     highlight NonText guibg=None guifg=None
+"     highlight EndOfBuffer guifg=#1B253B
+"     highlight CursorLine guibg=#1B253B
+"     highlight CursorLineNr guibg=#1B253B guifg=#73a7a7
+"     highlight VertSplit gui=NONE guibg=None guifg=#10737B
+"     highlight IndentBlanklineChar guifg=#1B253B gui=nocombine
+"     highlight IndentBlanklineContextChar guifg=#243353 gui=nocombine
+"     highlight FloatermBorder guibg=None guifg=#10737B
+"     highlight BufferLineSeparator guifg=#172030
+"     highlight TroubleSignOther guibg=None guifg=green
+"     highlight TroubleFoldIcon guifg=None
+"     highlight TroubleCount guibg=None
+"     highlight TelescopeBorder guifg=#243353
+"     highlight TelescopePromptBorder guifg=#243353
+"     highlight TelescopeResultsBorder guifg=#243353
+"     highlight EndOfBuffer guifg=#172030
+" endfunction
+" augroup Colors
+"     autocmd!
+"     autocmd ColorScheme carbon call CarbonOverrides()
+" augroup END
 
 " -------------------------------------- Color Scheme --------------------------------- "
 
-colorscheme carbon
+colorscheme dracula
