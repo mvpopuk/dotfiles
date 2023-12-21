952e879c281be2b420b26c348ab0d5a7f918543b
Marian Pop <mvpopuk@gmail.com>
2023-12-22 01:16:29 +0200

    Add indent guide for current scope

--- nvim/config/indent-blankline.lua
+++ nvim/config/indent-blankline.lua
@@ -1,6 +1,23 @@
-local highlight = { Number },
+local highlight = {
+    "Highlight",
+}
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "Highlight", { fg = "#001e36" })
+end)
+
 require("ibl").setup { 
-    scope = { enabled = false },
+    scope = { 
+       enabled = true,
+       show_start = true,
+       show_end = false,
+       injected_languages = false,
+       highlight = { "Function", "Label" },
+       priority = 500,
+    },
     indent = { highlight = highlight, char = "│" },
     exclude = {
         filetypes = {
